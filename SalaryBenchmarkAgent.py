a7826f8a8b01e30abe002f6a7745b13ef8a245cd
Vethanarrthana <147983328+Vethanarrthana@users.noreply.github.com>
2025-07-26 12:26:54 +0530

    Update SalaryBenchmarkAgent.py

--- SalaryBenchmarkAgent.py
+++ SalaryBenchmarkAgent.py
@@ -76,8 +76,8 @@ st.set_page_config(page_title="AI Salary Benchmarking", layout="centered")
 st.title("📄 AI Resume Salary Benchmarking")
 st.markdown("Upload your resume and get an **estimated salary** based on your experience and skills.")
 
-#os.environ["GROQ_API_KEY"] = "your api key"
-os.environ["GROK_API_KEY"]=st.secrets["GROQ_API_KEY"]
+os.environ["GROQ_API_KEY"] = "your api key"
+os.environ["GROK_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 llm = ChatGroq(
     model_name="groq/llama3-8b-8192",
